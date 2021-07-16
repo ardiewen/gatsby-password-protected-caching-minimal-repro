71b75e9b5dc6f20c56e7de9df210d819bb938d73
Ardie Wen <ardie.wen@hunterlily.co>
2021-07-16 10:43:34 -0400

    feat: mergeLinkHeaders

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,9 +32,10 @@ module.exports = {
     {
       resolve: `gatsby-plugin-gatsby-cloud`,
       options: {
+        mergeLinkHeaders: false,
         mergeCachingHeaders: false,
-    },  
-  },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
